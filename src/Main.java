96d94ab57874cbbb8e56a0184404e28126e83556
maxchtse <tsechunghei97@outlook.com>
2024-01-12 21:59:12 +0800

    Create a 400x400 window

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,30 @@
+import javax.swing.*;
+import java.awt.*;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+public class Main extends JPanel {
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+    public static int width = 400;
+    public static int height = 400;
+    public Main() {
+
+    }
+    @Override
+    public Dimension getPreferredSize() {
+        return new Dimension(width, height);
+    }
+
+    public static void main(String[] args) {
+        JFrame window = new JFrame("Snake Game");
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setContentPane(new Main());
+        window.pack();
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+        window.setResizable(false);
+
+
+
     }
 }
