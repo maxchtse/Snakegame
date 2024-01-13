cac2dddb1e3e9fef4b495ac83d2788b373ab2724
maxchtse <tsechunghei97@outlook.com>
2024-01-13 14:38:27 +0800

    Create SnakeBody

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,24 @@ import java.awt.*;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main extends JPanel {
-
+    public static final int CELL_SIZE = 20;
     public static int width = 400;
     public static int height = 400;
-    public Main() {
+    public static int row = height / CELL_SIZE;
+    public static int col = width / CELL_SIZE;
+    private Snake snake;
 
+    public Main() {
+        snake = new Snake();
     }
+
+    @Override
+    public void paintComponent(Graphics g) {
+        // Draw a black BG
+        g.fillRect(0, 0, width, height);
+        snake.drawSnake(g);
+    }
+
     @Override
     public Dimension getPreferredSize() {
         return new Dimension(width, height);
@@ -23,8 +35,5 @@ public class Main extends JPanel {
         window.setLocationRelativeTo(null);
         window.setVisible(true);
         window.setResizable(false);
-
-
-
     }
 }
---
+++ src/Snake.java
@@ -0,0 +1,22 @@
+import java.awt.*;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class Snake {
+    private ArrayList<Node> snakeBody;
+
+    public Snake() {
+        snakeBody = new ArrayList<>();
+        snakeBody.add(new Node(80, 0));
+        snakeBody.add(new Node(60, 0));
+        snakeBody.add(new Node(40, 0));
+        snakeBody.add(new Node(20, 0));
+    }
+
+    public void drawSnake(Graphics g) {
+        for (Node n: snakeBody) {
+            g.setColor(Color.cyan);
+            g.fillOval(n.x, n.y, Main.CELL_SIZE, Main.CELL_SIZE);
+        }
+    }
+}
